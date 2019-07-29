443168f6a54b0690e2c26fb12ad38b64798e8384
cihatturhan <cihatturhan@192.168.32.6>
2019-07-29 23:49:05 +0300

    first commmit Spring Security Tamplate

---
+++ hrapp/src/main/java/com/simpleapplication/hrjoblisting/HrjoblistingApplication.java
@@ -0,0 +1,13 @@
+package com.simpleapplication.hrjoblisting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrjoblistingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrjoblistingApplication.class, args);
+	}
+
+}
---
+++ hrapp/src/test/java/com/simpleapplication/hrjoblisting/HrjoblistingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.simpleapplication.hrjoblisting;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HrjoblistingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
